7f7084bfc483c6393c333c4bb0aa48f5c611307f
Peter K <pkjar16@student.aau.dk>
2020-10-09 13:26:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,7 @@ def say_hello():
 
     send_node_status(request.headers, request.get_json())
     return 'Hello from Server'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '127.0.0.1',port=5000)
