69c62f095d3bb08a7f059659cfe53bf2b114a6f2
Mads <madhan16@student.aau.dk>
2020-10-21 13:21:52 +0200

    added multithreading and SHA256 hashing

--- app.py
+++ app.py
@@ -1,18 +1,53 @@
 from flask import Flask
 from flask import request
 import requests
+import concurrent.futures
+import hashlib
 
 app = Flask(__name__)
 main_queue = []
 send_queue = []
 
+def unpack_and_send(request):
+    # package_type = request.headers["package_type"]
+    # from_node = request.headers["nodeid"]
+    # from_ip = request.headers["ip-address"]
+    # token = request.headers["auth-token"]
+    # message = request.get_json()
+    resources = []
+    packages = []
+    processes = []
 
-@app.route('/')
-def index():
-    return 'Server Works!'
+    request_list = request.get_json()
+
+    for item in request_list:
+        if item[0]['package_type'] == '1':
+            resources.append(item)
+        elif item[0]['package_type'] == '2':
+            packages.append(item)
+        elif item[0]['package_type'] == '3':
+            processes.append(item)
+
+
+    if send_hash(resources[0][0], hashlib.sha256(resources).hexdigest()) == 200:
+        resources_status = send_node_status(resources[0][0], resources)
+
+    if send_hash(packages[0][0], hashlib.sha256(packages).hexdigest()) == 200:
+        packages_status = send_node_status(packages[0][0], packages)
+
+    if send_hash(processes[0][0], hashlib.sha256(processes).hexdigest()) == 200:
+        processes_status = send_node_status(processes[0][0], processes)
+
+    if resources_status & packages_status & processes_status == 200:
+        return resources_status
+    else:
+
+        return "Failed to send to host, resend request"
+
+
 
 def send_hash(old_headers, message):
-    url = "Hash endpoint url here"
+    url = "http://217.69.10.141:5000/node-hash"
     headers = {'Content-Type': 'application/json',
                'Accept': 'text/plain',
                'auth-token': old_headers['auth-token']
@@ -42,46 +77,15 @@ def send_node_status(old_headers, message):
     return r.status_code
 
 
+@app.route('/')
+def index():
+    return 'Server Works!'
+
+
 @app.route('/sendtohost', methods=['POST'])
-def unpack_and_send():
-    # package_type = request.headers["package_type"]
-    # from_node = request.headers["nodeid"]
-    # from_ip = request.headers["ip-address"]
-    # token = request.headers["auth-token"]
-    # message = request.get_json()
-    resources = []
-    packages = []
-    processes = []
-
-    request_list = request.get_json()
-
-    for item in request_list:
-        if item[0]['package_type'] == '1':
-            resources.append(item)
-        elif item[0]['package_type'] == '2':
-            packages.append(item)
-        elif item[0]['package_type'] == '3':
-            processes.append(item)
-
-
-    resources_hash = hash(resources)
-    packages_hash = hash(packages)
-    processes_hash = hash(processes)
-
-    if send_hash(resources[0][0], resources_hash) == 200:
-        resources_status = send_node_status(resources[0][0], resources)
-
-    if send_hash(packages[0][0], packages_hash) == 200:
-        packages_status = send_node_status(packages[0][0], packages)
-
-    if send_hash(processes[0][0], processes_hash) == 200:
-        processes_status = send_node_status(processes[0][0], processes)
-
-    if resources_status & packages_status & processes_status == 200:
-        return resources_status
-    else:
-
-        return "Failed to send to host, resend request"
+def send_data():
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        return executor.submit(unpack_and_send(request)).result() 
 
 @app.route('/sendtoleader', methods=['POST'])
 def leader_send():
