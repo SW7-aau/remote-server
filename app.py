201f1326a4fb9eddd973051c7397fb8dd7d0258e
Peter K <pkjar16@student.aau.dk>
2020-10-26 13:26:46 +0100

    fix

--- app.py
+++ app.py
@@ -120,10 +120,11 @@ def send_hash(old_headers, message):
     url = "http://217.69.10.141:5000/node-hash" #node hash url
     headers = {'Content_Type': 'application/json',
                'Accept': 'text/plain',
-               'access_token': token
+               'access_token': 'fuk'
                }
+    
     r = requests.post(url, json=message, headers=headers)
-
+    print(r.json())
     if r.json()['message'] == 'ok':
         return r.status_code
     elif r.json()['message'] == 'token_expired':
