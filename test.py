12602cf748b1210bd1089e88b5e88a7c3603c2ee
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2020-11-05 20:10:04 +0100

    fixed bad .json() test

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 
-config = requests.get('http://217.69.10.141:5000/get-config?cluster_id=' + '3000').json().json()
+config = requests.get('http://217.69.10.141:5000/get-config?cluster_id=' + '3000').json()
 print(config)
 print([*config])
