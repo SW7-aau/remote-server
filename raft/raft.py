c50bffa52c1bc20a15315363ce15a8774d99168d
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2020-11-05 23:45:22 +0100

    RequestVote failed because silly me didn't process ips into urls

--- raft/raft.py
+++ raft/raft.py
@@ -169,7 +169,7 @@ class Node:
         """
         response = int(requests.get(url + '/requestvote',
                                     headers=self.create_headers(),
-                                    timeout=5).text)
+                                    timeout=0.15).text)
         self.votes += response
         if self.verbosity == 1:
             print('votes: ', str(self.votes))
