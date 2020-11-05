fb1bca8513620ac8391d3418f47bf82342e960cc
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2020-11-05 20:09:05 +0100

    kektest

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 
-config = requests.get('http://217.69.10.141:5000/get-config?cluster_id=' + '3000').json()
+config = requests.get('http://217.69.10.141:5000/get-config?cluster_id=' + '3000').json().json()
 print(config)
 print([*config])
