7017871647c8d3c9d24ed511ea618b14b5c47273
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2020-11-23 06:06:51 +0100

    Update mock_requests.py

--- mock_requests.py
+++ mock_requests.py
@@ -2,8 +2,10 @@ import mock_app
 
 class requests():
     def post(self, url, json, headers):
+        result = 0
         if(url == "http://127.0.0.1:5000/sendtohost"):
-            return self.Response(mock_app.say_hello(headers=headers, json=json))
+            result = mock_app.say_hello(headers=headers, json=json)
+        return self.Response(result)
     class Response():
         def __init__(self, status_code):
             self.status_code = status_code
