99a4fa7d588973d21ff0c2eb5219cdba9609573a
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2020-12-06 23:41:21 +0100

    mock drafts for app and river WIP

--- mock_app.py
+++ mock_app.py
@@ -1,45 +1,170 @@
+from copy import deepcopy 
+import mock_river
+import concurrent.futures
+import hashlib
+import base64
+import json
+import argparse
 import mock_nodes
 
 class requests():
     def post(self, url, json, headers):
-        if(url == "http://217.69.10.141:5000/node-resources"):
-            return self.Response(mock_nodes.node_resources(message=json))
-        if(url == "http://217.69.10.141:5000/node-network"):
-            return self.Response(mock_nodes.node_network(json))
+        print()
+    def get(self, url, json, headers)
     class Response():
         def __init__(self, status_code):
             self.status_code = status_code
 
-def index():
-    return 'Server Works!'
+def arg_parsing():
+    parser = argparse.ArgumentParser(prog='Read Packets',
+                                     description='Read Network Packets')
+    parser.add_argument('-i', '--ip-address',
+                        help='IP Address of the current node.')
+    parser.add_argument('-c', '--cluster-id', type=int,
+                        help='ID of the cluster the current node is in.')
+    parser.add_argument('-p', '--port', type=int,
+                        help='The port the current node is using.')
+    parser.add_argument('-v', '--verbosity', type=int, default=1,
+                        help='Increase output verbosity.')
 
-def send_node_status(old_headers, message):
+    return parser.parse_args()
+
+args = arg_parsing()
+requests = requests()
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    node = mock_river.Node(executor, args)
+
+def send_data():
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        while node.leader_queue:
+            executor.submit(unpack_and_send, node.leader_queue.pop(0))
+
+    return 'ok'
+
+def unpack_and_send(queue):
+    """
+    Unpacks a send_queue and sends it to GCP
+    :param queue: A send_queue from a follower
+    :return: Nothing
+    """
+    resources_status = None
+    packages_status = None
+    resources_hash_status = None
+    packages_hash_status = None
+    resources = []
+    packages = []
+
+    for item in queue:
+        if item[0]['package_type'] == '1':
+            resources.append(item)
+        elif item[0]['package_type'] == '2':
+            packages.append(item)
+
+    if resources:
+        b64 = base64.encodebytes(json.dumps(resources).encode())
+        hashed_resources = hashlib.sha256(b64).hexdigest()
+        resources_hash_status = check_hash(resources[0][0], hashed_resources)
+
+    if packages:
+        b64 = base64.encodebytes(json.dumps(packages).encode())
+        hashed_packages = hashlib.sha256(b64).hexdigest()
+        packages_hash_status = check_hash(packages[0][0], hashed_packages)
+
+    if resources_hash_status == 200:
+        resources_status = send_to_gcp(resources[0][0], resources)
+        send_hash(hashed_resources)
+
+    if packages_hash_status == 200:
+        packages_status = send_to_gcp(packages[0][0], packages)
+        send_hash(hashed_packages)
+
+    if ((resources_status == 200 or resources_status is None) and (packages_status == 200 or packages_status is None)) or ((resources_hash_status == 1 or resources_hash_status is None) and (packages_hash_status == 1 or packages_hash_status is None)):
+        url = 'http://' + queue[0][0]['ip_address'] + ':' + node.port + '/datasent'
+        print("data sent response sent to " + url)
+        headers = {'leader_ip_address': node.ip}
+
+        r = requests.get(url, headers=headers)
+        if args.verbosity == 2:
+            print(r)
+        if r.json()['message'] != 'ok':
+            print('Not leader')
+
+
+def check_hash(old_headers, message):
+    """
+    Checks if the hash is already in the database at GCP
+    :param old_headers: Headers from the send_queue
+    :param message: A hashed version of the send_queue
+    :return: status_code from the request if successful, 1 if hash exists in
+     database, 0 if access token has expired
+    """
+
+    url = "http://95.179.226.113:5000/check-hash"  # node hash url
+    headers = {'Content_Type': 'application/json',
+               'Accept': 'text/plain',
+               'access_token': node.token,
+               'cluster_id': node.cluster_id
+               }
+
+    r = requests.post(url, json=message, headers=headers)
+
+    if r.status_code == 409:
+        return 1
+    if r.json()['message'] == 'ok':
+        return r.status_code
+    elif r.json()['message'] == 'Authorization token is expired':
+        node.get_auth_token()
+        return 0
+    elif r.json()['message'] == 'Not authorized':
+        node.become_follower()
+        return 0
+
+def send_hash(message):
+    url = "http://95.179.226.113:5000/insert-hash"
+    headers = {'Content_Type': 'application/json',
+               'Accept': 'text/plain',
+               'access_token': node.token,
+               'cluster_id': node.cluster_id
+               }
+
+    r = requests.post(url, json=message, headers=headers)
+
+    if r.json()['message'] != 'ok':
+        node.get_auth_token()
+        return 0
+
+    return r.status_code
+
+def send_to_gcp(old_headers, message):
+    """
+    Sends the send_queue to GCP
+    :param old_headers: Headers from the send_queue
+    :param message: The data to be stored in the database
+    :return: status_code from the request
+    """
     if old_headers['package_type'] == '1':
-        url = "http://217.69.10.141:5000/node-resources"
+        url = 'http://95.179.226.113:5000/node-resources'
     elif old_headers['package_type'] == '2':
-        url = "http://217.69.10.141:5000/node-network"
-    elif old_headers['package_type'] == '3':
-        url = "http://217.69.10.141:5000/node-proc"
+        url = 'http://95.179.226.113:5000/node-network'
+
     else:
         return
 
     print('Sent to: ', old_headers['package_type'])
-    headers = {'Content-Type': 'application/json',
+    headers = {'Content_Type': 'application/json',
                'Accept': 'text/plain',
-               'auth-token': old_headers['auth-token'],
-               'nodeid': old_headers['nodeid'],
-               'ip-address': old_headers['ip-address']}
+               'access_token': node.token,
+               'cluster_id': node.cluster_id
+               }
     r = requests.post(url, json=message, headers=headers)
-    return r.status_code
+
+    if r.json()['message'] == 'ok':
+        return r.status_code
+    elif r.json()['message'] == 'Authorization token is expired':
+        node.get_auth_token()
+        return 0
+    elif r.json()['message'] == 'Not authorized':
+        node.become_follower()
+        return 0
 
 
-def say_hello(headers, json):
-    # package_type = request.headers["package_type"]
-    # from_node = request.headers["nodeid"]
-    # from_ip = request.headers["ip-address"]
-    # token = request.headers["auth-token"]
-    # message = request.get_json()
-
-    return send_node_status(headers, json)
-
-requests = requests()
--- mock_read_packets.py
+++ mock_read_packets.py
@@ -25,22 +25,6 @@ class read_packets():
     packets_dict_list = []
     requests = mock_requests.requests()
 
-    def other_to_dict(self, packet, layer_name):
-        d = {}
-        if layer_name == 'llc':  # Protocol = STP
-            d = {'timestamp': packet.sniff_timestamp.split('.')[0],
-                'protocol': packet[2].layer_name.upper(),
-                'size': str(packet.length), 'info': {}}
-        elif layer_name == 'arp' or layer_name == 'eapol':  # Protocol = ARP or EAPOL
-            d = {'timestamp': packet.sniff_timestamp.split('.')[0],
-                'protocol': packet[1].layer_name.upper(),
-                'size': str(packet.length), 'info': {}}
-
-        d['info']['dst_resolved'] = str(packet[0].addr_oui_resolved)
-        d['info']['src_resolved'] = str(packet[0].src_oui_resolved)
-
-        return d
-
 
     def get_packets(self, packet, verbosity):
         d = {}
@@ -60,21 +44,8 @@ class read_packets():
                     print('Unknown package')
 
         except AttributeError:
-            layer_name = str(packet[1].layer_name)
-            # d = other_to_dict(packet, layer_name)
-
-            if verbosity == 1:
-                if layer_name == 'llc':  # Protocol = STP
-                    print('STP')
-                elif layer_name == 'arp':  # Protocol = ARP
-                    print('ARP')
-                elif layer_name == 'eapol':  # Protocol = EAPOL
-                    print('EAPOL')
-                else:
-                    print('Unknown package')
-
-        if not d and verbosity == 1:  # Used to find undiscovered protocols
-            print("Empty dict")
+            if verbosity == 1:  # Used to find undiscovered protocols
+                print("Empty dict")
 
         return d
 
@@ -82,10 +53,10 @@ class read_packets():
         url = "http://127.0.0.1:5000/sendtohost"
         headers = {'Content-type': 'application/json',
                 'Accept': 'text/plain',
-                'auth-token': 'testtoken',
-                'package_type': '2',
-                'nodeid': 'testid',
-                'ip-address': "127.0.0.1"}
-        print(json_object)
+                'package_type': '2'}
         r = self.requests.post(url, json=json_object, headers=headers)
         return r.status_code
+
+    def send_packets_list(self):
+        self.send_node_status(self.packets_dict_list)
+        self.packets_dict_list.clear()
