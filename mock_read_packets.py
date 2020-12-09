262aa016a60a5fdfd11534b18a904d283f1b8989
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2020-12-09 22:13:04 +0100

    Expanded packets class

--- mock_read_packets.py
+++ mock_read_packets.py
@@ -1,36 +1,36 @@
 import mock_requests
 
-def ip_to_dict(packet, protocol):
-    d = {'timestamp': packet.sniff_timestamp.split('.')[0],
-            'protocol': packet.transport_layer, 'size': str(packet.length),
-            'info': {}}
-    if str(protocol) == '2':  # Protocol = IGMP
-        d['info']['dst'] = str(packet[1].dst)
-        d['info']['dst_resolved'] = str(packet[0].addr_oui_resolved)
-        d['info']['src'] = str(packet[1].src)
-        d['info']['src_resolved'] = str(packet[0].src_oui_resolved)
-        d['info']['layer'] = packet[2].layer_name
-    else:
-        d['info']['dst'] = str(packet[1].dst)
-        #d['info']['dst_resolved'] = str(packet[0].addr_oui_resolved)
-        d['info']['dst_port'] = packet[2].dstport
-        d['info']['src'] = str(packet[1].src)
-        #d['info']['src_resolved'] = str(packet[0].src_oui_resolved)
-        d['info']['src_port'] = packet[2].srcport
-        if protocol == '17':  # Protocol = UPD  ---- Else TCP
-            d['info']['layer'] = packet[3].layer_name
-    return d
 
 class read_packets():
     packets_dict_list = []
     requests = mock_requests.requests()
 
+    def ip_to_dict(self, packet, protocol):
+        d = {'timestamp': packet.sniff_timestamp.split('.')[0],
+                'protocol': packet.transport_layer, 'size': str(packet.length),
+                'info': {}}
+        if str(protocol) == '2':  # Protocol = IGMP
+            d['info']['dst'] = str(packet[1].dst)
+            d['info']['dst_resolved'] = str(packet[0].addr_oui_resolved)
+            d['info']['src'] = str(packet[1].src)
+            d['info']['src_resolved'] = str(packet[0].src_oui_resolved)
+            d['info']['layer'] = packet[2].layer_name
+        else:
+            d['info']['dst'] = str(packet[1].dst)
+            #d['info']['dst_resolved'] = str(packet[0].addr_oui_resolved)
+            d['info']['dst_port'] = packet[2].dstport
+            d['info']['src'] = str(packet[1].src)
+            #d['info']['src_resolved'] = str(packet[0].src_oui_resolved)
+            d['info']['src_port'] = packet[2].srcport
+            if protocol == '17':  # Protocol = UPD  ---- Else TCP
+                d['info']['layer'] = packet[3].layer_name
+        return d
 
     def get_packets(self, packet, verbosity):
         d = {}
         try:
             protocol = str(packet[1].proto)
-            d = ip_to_dict(packet, protocol)
+            d = self.ip_to_dict(packet, protocol)
             self.packets_dict_list.append(d)
 
             if verbosity == 1:
