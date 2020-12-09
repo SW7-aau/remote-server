3a7b82aaba92cc5c14f1ef01cf489ee015e58445
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2020-12-09 05:39:30 +0100

    Update mock_requests.py

--- mock_requests.py
+++ mock_requests.py
@@ -4,7 +4,7 @@ class requests():
     def post(self, url, json={}, headers={}):
         result = 0
         if(url == "http://127.0.0.1:5000/storedata"):
-            result = mock_app.information_queue(request(headers=headers, json=json))
+            result = mock_app.information_queue(self.request(headers=headers, json=json))
             if result == 'Data Appended':
                 return self.Response(200)
         return self.Response(500)
@@ -12,9 +12,9 @@ class requests():
         def __init__(self, status_code):
             self.status_code = status_code
     class request():
-        def __init__(self, json={}, headers={})
+        def __init__(self, json={}, headers={}):
             self.json = json
             self.headers = headers
 
-        def get_json():
+        def get_json(self):
             return self.json
